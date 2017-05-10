cc679c44d3b36ed6e7fbc0e2867eb9036622e3f5
Evgeniy Amosov <efreet@efreet.ru>
2017-05-10 08:21:49 +0300

    inherited TDoc

--- lib/java/src/org/apache/thrift/TDoc.java
+++ lib/java/src/org/apache/thrift/TDoc.java
@@ -8,9 +8,11 @@ import static java.lang.annotation.ElementType.TYPE;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.annotation.Inherited;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({TYPE, FIELD, PARAMETER, METHOD})
+@Inherited
 public @interface TDoc {
 	String name() default "";
 	String value() default "";
